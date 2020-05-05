4506b8502060aa592053e766a0dd3b225306f0e0
andrin.sibold@stud.bbbaden.ch <Andrinspeedy12>
2020-05-05 14:10:04 +0200

    removed unused class

--- src/main/java/ch/bbbaden/m151/GlücksradApplication.java
+++ src/main/java/ch/bbbaden/m151/GlücksradApplication.java
@@ -1,16 +1,9 @@
 package ch.bbbaden.m151;
 
-import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.DispatcherType;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import javax.faces.webapp.FacesServlet;
-import java.util.EnumSet;
 
 @SpringBootApplication
 public class GlücksradApplication {
@@ -22,19 +15,5 @@ public class GlücksradApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public ServletRegistrationBean servletRegistrationBean() {
-		FacesServlet servlet = new FacesServlet();
-		return new ServletRegistrationBean(servlet, "*.jsf");
-	}
-	@Bean
-	public FilterRegistrationBean rewriteFilter() {
-		FilterRegistrationBean rwFilter = new FilterRegistrationBean(new RewriteFilter());
-		rwFilter.setDispatcherTypes(EnumSet.of(DispatcherType.FORWARD, DispatcherType.REQUEST,
-				DispatcherType.ASYNC, DispatcherType.ERROR));
-		rwFilter.addUrlPatterns("/*");
-		return rwFilter;
 	}
 }
